2d4070a9d1e4abead4ee644a7bda1970539ae9f2
LENOVO <LENOVO@LAPTOP-IKNRLI1D>
2023-05-17 17:05:08 +0530

    My First simple program commit 2

---
+++ DemoJavaProject2/src/examples/program/FirstProgram.java
@@ -0,0 +1,11 @@
+package examples.program;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello1..");
+		System.out.println("Welcome to git");
+	}
+
+}
